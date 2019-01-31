a7bf4eb3ab5404ddbc4dbec343c35ad546f0d1ac
Divya Sasidharan <hello@shortdiv.com>
2019-01-31 13:46:38 -0600

    add role based function redirect rule

--- src/lambda/super-special-function.js
+++ src/lambda/super-special-function.js
@@ -1,13 +1,7 @@
 export function handler(event, context, callback) {
-  const { identity, user } = context.clientContext;
-  const { headers } = event;
-  console.log("identity  ", identity);
-  console.log("userrrrrr", user);
-
   const parsedBody = JSON.parse(event.body);
   const { userData } = parsedBody;
-  console.log("userData", userData);
-  console.log("headerssss", headers);
+  console.log("userData", userData.app_metadata.roles);
   callback(null, {
     statusCode: 200,
     body: JSON.stringify({ msg: "Hello, this is a super secret function!" })
