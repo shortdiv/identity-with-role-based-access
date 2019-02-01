35c6fe3f1dbca995cdc13d16757f553c5bac67b2
Divya Sasidharan <hello@shortdiv.com>
2019-02-01 10:34:22 -0600

    testing function redirects

--- src/lambda/super-special-function.js
+++ src/lambda/super-special-function.js
@@ -1,6 +1,12 @@
+const cookie = require("cookie");
+
 export function handler(event, context, callback) {
+  const { headers } = event;
   const parsedBody = JSON.parse(event.body);
   const { userData } = parsedBody;
+  const cookieHeader = headers.cookie || "";
+  const cookies = cookie.parse(cookieHeader);
+  console.log(cookies);
   console.log("userData", userData.app_metadata.roles);
   callback(null, {
     statusCode: 200,
