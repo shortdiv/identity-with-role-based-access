c7f8bc8a1d7c98a8c9aed5471384cbbf0a4fe798
Divya Sasidharan <hello@shortdiv.com>
2019-02-04 14:57:01 -0600

    data organization

--- src/lambda/super-special-function.js
+++ src/lambda/super-special-function.js
@@ -3,11 +3,13 @@ const cookie = require("cookie");
 export function handler(event, context, callback) {
   const { headers } = event;
   const parsedBody = JSON.parse(event.body);
-  const { userData } = parsedBody;
+  const { params } = parsedBody;
+  const userData = params.userData;
   const cookieHeader = headers.cookie || "";
   const cookies = cookie.parse(cookieHeader);
   console.log(cookies);
   console.log("userData", userData.app_metadata.roles);
+
   callback(null, {
     statusCode: 200,
     body: JSON.stringify({ msg: "Hello, this is a super secret function!" })
