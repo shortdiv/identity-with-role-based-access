85744d78bab9c4e0d0591cf65a17975f6177874e
Divya Sasidharan <hello@shortdiv.com>
2019-02-05 16:17:52 -0600

    fix lambda

--- src/lambda/identity-signup.js
+++ src/lambda/identity-signup.js
@@ -3,15 +3,15 @@ exports.handler = function(event, context, callback) {
   console.log(context.clientContext);
   // const { user } = context.clientContext;
 
-  // const responseBody = {
-  //   app_metadata: {
-  //     roles: validateUser(user),
-  //     my_user_info: "this is user info that the user can't change from the UI"
-  //   },
-  //   user_metadata: {
-  //     custom_data_from_function: "hurray this is some extra metadata"
-  //   }
-  // };
+  const responseBody = {
+    app_metadata: {
+      roles: ["visitor"],
+      my_user_info: "this is user info that the user can't change from the UI"
+    },
+    user_metadata: {
+      custom_data_from_function: "hurray this is some extra metadata"
+    }
+  };
 
   // console.log("user ", user);
   // console.log("context ", context);
@@ -28,6 +28,6 @@ exports.handler = function(event, context, callback) {
 
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify({ msg: "Hello, World!" })
+    body: JSON.stringify(responseBody)
   });
 };
