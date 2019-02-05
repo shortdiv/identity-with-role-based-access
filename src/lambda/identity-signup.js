fdef67d3102b385a3ef844d58378266dbab9b5a6
Divya Sasidharan <hello@shortdiv.com>
2019-02-05 17:18:41 -0600

    fix i think

--- src/lambda/identity-signup.js
+++ src/lambda/identity-signup.js
@@ -2,9 +2,13 @@ exports.handler = function(event, context, callback) {
   const data = JSON.parse(event.body);
   const { user } = data;
 
+  console.log(user);
+  console.log("IUDK", event.body.user);
+  console.log("wtf event", event.body);
+
   const responseBody = {
     app_metadata: {
-      roles: validateUser(user.email),
+      roles: ["visitor"],
       my_user_info: "this is user info that the user can't change from the UI"
     },
     user_metadata: {
@@ -12,13 +16,13 @@ exports.handler = function(event, context, callback) {
     }
   };
 
-  const validateUser = email => {
-    if (email.split("@")[1] === "netlify.com") {
-      return ["editor"];
-    } else {
-      return ["visitor"];
-    }
-  };
+  // const validateUser = email => {
+  //   if (email.split("@")[1] === "netlify.com") {
+  //     return ["editor"];
+  //   } else {
+  //     return ["visitor"];
+  //   }
+  // };
 
   callback(null, {
     statusCode: 200,
