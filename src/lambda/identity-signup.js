84d3d5002f6c205f09e6d51c9c6d6de698efc5bd
Divya Sasidharan <hello@shortdiv.com>
2019-02-05 16:40:31 -0600

    fix: lambda signup

--- src/lambda/identity-signup.js
+++ src/lambda/identity-signup.js
@@ -1,31 +1,24 @@
 exports.handler = function(event, context, callback) {
-  console.log(context);
-  console.log(context.clientContext);
-  // const { user } = context.clientContext;
-  console.log(event.body);
+  const { user } = event.body;
 
   const responseBody = {
     app_metadata: {
-      roles: ["visitor"],
+      roles: validateUser(user.email),
       my_user_info: "this is user info that the user can't change from the UI"
     },
     user_metadata: {
+      ...user.app_metadata,
       custom_data_from_function: "hurray this is some extra metadata"
     }
   };
 
-  // console.log("user ", user);
-  // console.log("context ", context);
-
-  // const validateUser = user => {
-  //   if (user !== undefined) {
-  //     if (user.email.split("@")[1] === "netlify.com") {
-  //       return ["editor"];
-  //     }
-  //   } else {
-  //     return ["visitor"];
-  //   }
-  // };
+  const validateUser = email => {
+    if (email.split("@")[1] === "netlify.com") {
+      return ["editor"];
+    } else {
+      return ["visitor"];
+    }
+  };
 
   callback(null, {
     statusCode: 200,
