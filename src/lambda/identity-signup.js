3caac86daee0df25190a2b014cae78e041d38bc0
Divya Sasidharan <hello@shortdiv.com>
2019-02-05 16:12:52 -0600

    testing lambda

--- src/lambda/identity-signup.js
+++ src/lambda/identity-signup.js
@@ -1,31 +1,33 @@
 exports.handler = function(event, context, callback) {
-  const { user } = context.clientContext;
+  console.log(context);
+  console.log(context.clientContext);
+  // const { user } = context.clientContext;
 
-  const responseBody = {
-    app_metadata: {
-      roles: validateUser(user),
-      my_user_info: "this is user info that the user can't change from the UI"
-    },
-    user_metadata: {
-      custom_data_from_function: "hurray this is some extra metadata"
-    }
-  };
+  // const responseBody = {
+  //   app_metadata: {
+  //     roles: validateUser(user),
+  //     my_user_info: "this is user info that the user can't change from the UI"
+  //   },
+  //   user_metadata: {
+  //     custom_data_from_function: "hurray this is some extra metadata"
+  //   }
+  // };
 
-  console.log("user ", user);
-  console.log("context ", context);
+  // console.log("user ", user);
+  // console.log("context ", context);
 
-  const validateUser = user => {
-    if (user !== undefined) {
-      if (user.email.split("@")[1] === "netlify.com") {
-        return ["editor"];
-      }
-    } else {
-      return ["visitor"];
-    }
-  };
+  // const validateUser = user => {
+  //   if (user !== undefined) {
+  //     if (user.email.split("@")[1] === "netlify.com") {
+  //       return ["editor"];
+  //     }
+  //   } else {
+  //     return ["visitor"];
+  //   }
+  // };
 
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify(responseBody)
+    body: JSON.stringify({ msg: "Hello, World!" })
   });
 };
