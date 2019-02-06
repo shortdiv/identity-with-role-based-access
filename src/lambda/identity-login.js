d39788fa8b36e3655517e243a0815942db7bbee3
Divya Sasidharan <hello@shortdiv.com>
2019-02-06 17:44:29 -0600

    add login logic

---
+++ src/lambda/identity-login.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+export function handler(event, context, callback) {
+  const { email, full_name } = JSON.parse(event.body);
+  const { identity } = context.clientContext;
+  const referer = event.headers.referer;
+
+  console.log("this is a magical referer", referer);
+
+  console.log(email);
+  console.log(full_name);
+
+  const config = {
+    headers: {
+      Bearer: `Authorization ${identity.token}`
+    }
+  };
+  const postData = {
+    email: "divified@gmail.com",
+    password: "password",
+    full_name: "Robot Div"
+  };
+  axios.post("/.netlify/indentity/admin/users", postData, config).then(res => {
+    console.log(res);
+  });
+
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({ msg: "Hello, this is a message!" })
+  });
+}
