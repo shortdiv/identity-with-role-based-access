cb84ccbd31bf7b6ca8932fe4901930141d2b379d
Divya Sasidharan <hello@shortdiv.com>
2019-02-07 17:18:17 -0600

    post users

--- src/lambda/super-special-function.js
+++ src/lambda/super-special-function.js
@@ -1,55 +1,33 @@
 const axios = require("axios");
 
 export function handler(event, context) {
-  console.log("HELLO THERE");
-  console.log(event.body);
   const { identity } = context.clientContext;
 
-  console.log("I am an identity", identity.token);
-
-  const generatePost = async function() {
-    const config = {
-      headers: {
-        Bearer: `Authorization ${identity.token}`
-      }
-    };
-    const postData = {
-      email: "divified@gmail.com",
-      password: "password",
-      confirm: true,
-      app_metadata: {
-        roles: ["editor"]
-      },
-      user_metadata: {
-        full_name: "Robot Div"
-      }
-    };
-
-    try {
-      const resp = await axios.post(
-        "/.netlify/identity/admin/users",
-        JSON.stringify(postData),
-        config
-      );
-      console.log(resp);
-    } catch (err) {
-      console.log("I AM AN ERROR", err);
-    }
+  const users = () => {
+    return axios({
+      method: "GET",
+      url: `${identity.url}/admin/users`,
+      headers: { Authorization: `Bearer ${identity.token}` }
+    });
   };
 
-  return generatePost();
-  // const data = {
-  //   email: "divified@gmail.com",
-  //   password: "qwerty",
-  //   confirm: true,
-  //   user_metadata: {
-  //     full_name: "ROBOT DIV"
-  //   }
-  // };
-  // return axios({
-  //   method: "POST",
-  //   url: `${identity.url}/admin/users`,
-  //   headers: { Authorization: `Bearer ${identity.token}` },
-  //   data: JSON.stringify(data)
-  // });
+  console.log(users);
+
+  const data = {
+    email: "divified@gmail.com",
+    password: "qwerty",
+    confirm: true,
+    app_metadata: {
+      roles: ["editor"]
+    },
+    user_metadata: {
+      full_name: "Robot Div"
+    }
+  };
+  return axios({
+    method: "POST",
+    url: `${identity.url}/admin/users`,
+    headers: { Authorization: `Bearer ${identity.token}` },
+    data: JSON.stringify(data)
+  });
 }
