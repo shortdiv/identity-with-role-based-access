d2751674f0a55b8b111c3e6fa21ff0d78b82d864
Divya Sasidharan <hello@shortdiv.com>
2019-02-08 15:58:21 -0600

    fixing role based function call

--- src/lambda/super-special-function.js
+++ src/lambda/super-special-function.js
@@ -1,46 +1,51 @@
 const axios = require("axios");
 
-export function handler(event, context) {
-  const { identity } = context.clientContext;
-
-  console.log("IDENT", identity);
-  console.log(context.clientContext.user);
-
+export function handler(event, callback) {
+  const referer = event.headers.referer;
   const { userData } = JSON.parse(event.body);
-  console.log("EVENT", userData);
+  const roles = userData.app_metadata.roles;
 
-  const users = () => {
+  //check roles!
+  if (roles.indexOf("editor")) {
+    // do some cool stuff
+    const attrs = {
+      user_metadata: {
+        ...userData.user_metadata,
+        last_login: new Date().toISOString()
+      }
+    };
+
     return axios({
-      method: "GET",
-      url: `${identity.url}/admin/users`,
-      headers: { Authorization: `Bearer ${identity.token}` }
+      url: `/.identity/admin/users/${userData.id}`,
+      method: "PUT",
+      body: JSON.stringify(attrs)
     });
-  };
-
-  users()
-    .then(res => {
-      console.log(res);
-    })
-    .catch(err => {
-      console.log("ERRORERER ", err);
+  } else {
+    return callback(null, {
+      statusCode: 401,
+      headers: {
+        Location: `${referer}`,
+        "Cache-Control": "no-cache"
+      },
+      body: JSON.stringify({ message: "User does not have access privileges" })
     });
-
-  const data = {
-    email: "divified@gmail.com",
-    password: "qwerty",
-    confirm: true,
-    role: "superstar",
-    app_metadata: {
-      roles: ["superstar"]
-    },
-    user_metadata: {
-      full_name: "Robot Div"
-    }
-  };
-  return axios({
-    method: "POST",
-    url: `${identity.url}/admin/users`,
-    headers: { Authorization: `Bearer ${identity.token}` },
-    data: JSON.stringify(data)
-  });
+  }
+  // const data = {
+  //   email: "divified@gmail.com",
+  //   password: "qwerty",
+  //   confirm: true,
+  //   role: "superstar",
+  //   app_metadata: {
+  //     roles: ["superstar"]
+  //   },
+  //   user_metadata: {
+  //     full_name: "Robot Div"
+  //   }
+  // };
+  // return axios({
+  //   method: "POST",
+  //   url: `${identity.url}/admin/users`,
+  //   headers: { Authorization: `Bearer ${identity.token}` },
+  //   data: JSON.stringify(data)
+  // });
 }
