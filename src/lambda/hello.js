63ccf95f3ca31ecb0d626be03372866b4037fb02
Divya Sasidharan <hello@shortdiv.com>
2019-02-07 18:02:02 -0600

    console log

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,5 +1,8 @@
 export function handler(event, context, callback) {
-  console.log(event);
+  console.log(context.clientContext.identity);
+  console.log(context.clientContext.user);
+  console.log(event.body);
+  console.log("userrrrr ", event.body.user);
   callback(null, {
     statusCode: 200,
     body: JSON.stringify({ msg: "Hello, World!" })
