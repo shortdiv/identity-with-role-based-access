df7a23bdd4c4333c9c7ce3d87c76d13a75497666
Divya Sasidharan <hello@shortdiv.com>
2019-02-08 18:15:15 -0600

    getuserdata

--- src/state/modules/user.js
+++ src/state/modules/user.js
@@ -1,14 +1,24 @@
+import axios from "axios";
+
 export const state = {
   user: null
 };
 export const mutations = {
   SET_USER(state, val) {
+    console.log(state);
     state.user = val;
   }
 };
 export const getters = {};
 export const actions = {
   getUserData() {
+    axios({
+      url: `/.identity/admin/users/${state.user.id}`,
+      headers: { Authorization: `Bearer ${state.user.token.access_token}` },
+      method: "GET"
+    }).then(res => {
+      console.log(res);
+    });
     console.log("hi im elfo");
   }
 };
