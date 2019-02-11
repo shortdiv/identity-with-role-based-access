3b561439013171f166dd30a5880a1f1314540451
Divya Sasidharan <hello@shortdiv.com>
2019-02-11 15:11:17 -0600

    return 200 on login

--- src/lambda/super-special-function.js
+++ src/lambda/super-special-function.js
@@ -1,29 +1,32 @@
-const axios = require("axios");
-
 export function handler(event, context, callback) {
   const referer = event.headers.referer;
   const { userData } = JSON.parse(event.body);
   console.log(userData);
   console.log("event", event.body);
   const { identity } = context.clientContext;
+  console.log(identity);
 
   const roles = userData.app_metadata.roles;
 
   //check roles!
   if (roles.indexOf("editor")) {
     // do some cool stuff
-    const attrs = {
-      user_metadata: {
-        ...userData.user_metadata,
-        last_login: new Date().toISOString()
-      }
-    };
+    // const attrs = {
+    //   user_metadata: {
+    //     ...userData.user_metadata,
+    //     last_login: new Date().toISOString()
+    //   }
+    // };
 
-    return axios({
-      url: `/.identity/admin/users/${userData.id}`,
-      headers: { Authorization: `Bearer ${identity.token}` },
-      method: "PUT",
-      body: JSON.stringify(attrs)
+    // return axios({
+    //   url: `/.identity/admin/users/${userData.id}`,
+    //   headers: { Authorization: `Bearer ${identity.token}` },
+    //   method: "PUT",
+    //   body: JSON.stringify(attrs)
+    // });
+    return callback(null, {
+      statusCode: 200,
+      body: JSON.stringify({ message: "hello there" })
     });
   } else {
     return callback(null, {
